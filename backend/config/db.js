53c5512c263eb741101ef1879b39373fc31da6e5
basakrittika2024@gmail.com <basakrittika2024@gmail.com>
2026-08-24 16:36:44 +0530

    Move MongoDB URI to .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,13 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async () => {
+//     await mongoose.connect('mongodb+srv://rittika27basak_db_user:VcZnmBdKdzL0KfR0@cluster0.lqlmust.mongodb.net/food-delivery').then(() => console.log("DB Connected"));
+// }
+
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rittika27basak_db_user:VcZnmBdKdzL0KfR0@cluster0.lqlmust.mongodb.net/food-delivery').then(() => console.log("DB Connected"));
+    await mongoose.connect(process.env.MONGODB_URI).then(() => console.log("DB Connected"));
 }
