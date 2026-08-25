0f4bc4025465531fc2cd59ed32aa1174a70bf035
basakrittika2024@gmail.com <basakrittika2024@gmail.com>
2026-08-25 11:13:16 +0530

    Fix cartItems undefined crash by defaulting to empty object

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -52,8 +52,9 @@ const StoreContextProvider = (props) => {
     }
 
     const loadCartData = async(token) => {
-        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
-        setCartItems(response.data.cartData);
+    const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+    setCartItems(response.data.cartData || {});
+
     }
 
     useEffect(()=>{
