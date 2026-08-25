179841dd6f933c8beb0f47d900b2b85d1eadc471
Rittika Basak <basakrittika2024@gmail.com>
2026-08-25 15:04:26 +0530

    Change API URL to production server
    Updated API URL for production deployment.

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://food-del-backend-p01o.onrender.com"
     const [token,setToken] = useState("")
     const[food_list,setFoodList] = useState([])
     const [searchQuery, setSearchQuery] = useState("")
